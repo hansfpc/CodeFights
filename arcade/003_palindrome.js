80eb1012c7bc2f45c791bc4818dd1cbfd3149c7f
hansfpc <hansfpc@outlook.com>
2018-03-01 01:18:34 -0300

    Solve 003

---
+++ arcade/003_palindrome.js
@@ -0,0 +1,35 @@
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+* */
+
+function checkPalindrome(inputString) {
+  let size = inputString.length;
+  if (size === 0) return false;
+  for (i=0; i < size / 2; i++){
+    if (inputString[i] !== inputString[(size - 1) - i]) return false;
+  }
+  return true
+}
